3f35b140558ca8b0a61b9e7631ec988799797805
Jared Tong <tongrhj@users.noreply.github.com>
2020-06-01 00:28:00 +0800

    Init project

---
+++ src/index.tsx
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+
+import css from './index.module.css'
+
+import ANIMpng from './ANIM.png'
+import Fpng from './F.png'
+import Hpng from './H.png'
+import KApng from './KA.png'
+import MALpng from './MAL.png'
+import Opng from './O.png'
+import STANpng from './STAN.png'
+import ZAKpng from './ZAK.png'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{ padding: '1rem 1.6rem'}}>
+        <Draggable>
+          <img style={{ marginLeft: '21%', marginTop: '15%' }} src={Hpng} alt="" draggable="false" />
+        </Draggable>
+        <Draggable>
+          <img style={{ marginLeft: '5%', marginTop: '0%' }} src={Opng} alt="" draggable="false" />
+        </Draggable>
+        <Draggable>
+          <img style={{ marginLeft: '5%', marginTop: '0%' }} src={MALpng} alt="" draggable="false" />
+        </Draggable>
+        <Draggable>
+          <img style={{ marginLeft: '5%', marginTop: '0%' }} src={STANpng} alt="" draggable="false" />
+        </Draggable>
+        <Draggable>
+          <img style={{ marginLeft: '0%', marginTop: '5%' }} src={Fpng} alt="" draggable="false" />
+        </Draggable>
+        <div style={{ marginTop: '-5px' }}>
+          <Draggable>
+            <img style={{ marginLeft: '5%', marginTop: '0%' }} src={KApng} alt="" draggable="false" />
+          </Draggable>
+          <Draggable>
+            <img style={{ marginLeft: '0%', marginTop: '0%' }} src={ANIMpng} alt="" draggable="false" />
+          </Draggable>
+          <Draggable>
+            <img style={{ marginLeft: '5%', marginTop: '0%' }} src={ZAKpng} alt="" draggable="false" />
+          </Draggable>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
